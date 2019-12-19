b4cc2f438a2c96a4cf65a3df1b78d1cdb22b6a00
Anastasia Raeva <anastassia.raeva@gmail.com>
2019-12-19 16:33:05 +0300

    Added catalog page

---
+++ html/src/js/index.js
@@ -0,0 +1,35 @@
+var burgerMenu = document.querySelector('.header__burger-menu');
+var mobileMenu = document.querySelector('.header__list');
+var close = document.querySelector('.header__close');
+var filterButton = document.querySelector('.filters__title');
+var filterForm = document.querySelector('.filters__form');
+var filterName = document.querySelectorAll('.filters__name');
+var filterSet = document.querySelectorAll('.filters__wrap');
+var filterRange = document.querySelector('.filters__range');
+
+burgerMenu.addEventListener('click', function(){
+    mobileMenu.classList.add('header__list--show');
+    burgerMenu.classList.toggle('header__burger-menu--none');
+    close.classList.add('header__close--show');
+});
+
+close.addEventListener('click', function(){
+    mobileMenu.classList.remove('header__list--show');
+    close.classList.remove('header__close--show');
+    burgerMenu.classList.toggle('header__burger-menu--none');
+});
+
+filterButton.addEventListener('click', function(){
+    filterButton.classList.toggle('filters__title--open');
+    filterForm.classList.toggle('filters__form--show');
+});
+
+for (var i=0; i<filterName.length; i++) {
+    filterName[i].addEventListener('click', function(){
+        this.classList.toggle('filters__name--open');
+        this.nextSibling.classList.toggle('filters__wrap--show');
+    });
+}
+
+
+
